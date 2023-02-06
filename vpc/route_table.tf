7cea3a960eb1d314d783fdc5c27c41dd9fe0fbef
akashn95 <akashn95.mec@gmail.com>
2023-02-06 05:27:47 +0000

    public route table

--- vpc/route_table.tf
+++ vpc/route_table.tf
@@ -1,8 +1,8 @@
 resource "aws_route_table" "main" {
+  
   vpc_id = aws_vpc.main.id
-
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
   }
 
@@ -12,6 +12,11 @@ resource "aws_route_table" "main" {
   }
 
   tags = {
-    Name = "main"
+    Name = "Public"
   }
+}
+
+resource "aws_main_route_table_association" "main" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.main.id
 }
