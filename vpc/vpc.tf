d587528e01534cb623e88456f159df5ff205d4e5
akashn95 <akashn95.mec@gmail.com>
2023-02-06 04:09:18 +0000

    subnet addition

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -6,3 +6,25 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "main" {
+  vpc_id	= aws_vpc.main.id
+  cidr_block	= "10.0.1.0/24"
+  
+  tags = {
+    Name = Public
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id        = aws_vpc.main.id
+  cidr_block    = "10.0.2.0/24"
+
+  tags = {
+    Name = Private
+  }
+}
+
+
+
+
